6d707c885541f6e76d52b71351f3cecf49900a9c
yelainab <ybuford201@west-mec.org>
2020-08-21 23:38:41 -0700

    added js effect
    a typewriting effect with javascript and css

---
+++ type.js
@@ -0,0 +1,24 @@
+const text = ['hello', 'welcome', 'I am Yelaina', '   '];
+
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type(){
+
+    if(count === text.length){
+        count = 0;
+    }
+
+    currentText = text[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector('.type').textContent = letter;
+    if(letter.length === currentText.length){
+        count++;
+        index = 0;
+    }
+    setTimeout(type, 400);
+
+}());
